3a5ef5cb8c4da60ce4ed0fba8daf625ac920d0e8
charles-cunningham <45041242+charles-cunningham@users.noreply.github.com>
2024-08-16 17:45:38 +0100

    Update 01_Process_BIOSYS.R
    Read in separate files

--- 01_Process_BIOSYS.R
+++ 01_Process_BIOSYS.R
@@ -34,20 +34,6 @@ if (!file.exists(dataDir)) {
 
 ### DOWNLOAD TAXON INFO DATA FROM DATA.GOV.UK SITE
 
-# TAXON INFO
-
-# Set download url
-url <- "https://environment.data.gov.uk/ecology/explorer/downloads/OPEN_DATA_TAXON_INFO.zip"
-
-# Set file name
-file_name <- "TaxonInfo"
-
-# Download
-download.file(url = url, destfile = paste0(dataDir, file_name, ".zip"), sep = "")
-
-# Unzip (file name = "OPEN_DATA_TAXON_INFO.csv")
-unzip(paste0(dataDir, "/", file_name, ".zip"), exdir = dataDir)
-
 # SURVEY DATA
 
 # Set download url
@@ -68,8 +54,33 @@ unlink(paste0(dataDir,
               c("TaxonInfo.zip",
                 "SurveyData.zip")))
 
+# TAXON INFO
+
+# Set download url
+url <- "https://environment.data.gov.uk/ecology/explorer/downloads/OPEN_DATA_TAXON_INFO.zip"
+
+# Set file name
+file_name <- "TaxonInfo"
+
+# Download
+download.file(url = url, destfile = paste0(dataDir, file_name, ".zip"), sep = "")
+
+# Unzip (file name = "OPEN_DATA_TAXON_INFO.csv")
+unzip(paste0(dataDir, "/", file_name, ".zip"), exdir = dataDir)
+
 ### READ IN BIOSYS DATA --------------------------------------------------------
 
-# Read in
-taxonInfo <- read.csv(paste0(dataDir, "OPEN_DATA_TAXON_INFO", ".csv"))
+# Read in separate files
+invMetrics <- read.csv(paste0(dataDir, "INV_OPEN_DATA_METRICS.csv"))
+invSite <- read.csv(paste0(dataDir, "INV_OPEN_DATA_SITE.csv"))
+invTaxa <- read.csv(paste0(dataDir, "INV_OPEN_DATA_TAXA.csv"))
+taxonInfo <- read.csv(paste0(dataDir, "OPEN_DATA_TAXON_INFO.csv"))
+
+# Join files together
+# N.B. Based on dataset documentation, the following can be used to join:
+# INV_OPEN_DATA_SITE.SITE_ID = INV_OPEN_DATA_METRICS.SITE_ID
+# INV_OPEN_DATA_METRICS.ANALYSIS_ID = INV_OPEN_DATA_TAXA.ANALYSIS_ID
+# OPEN_DATA_TAXON_INFO.TAXON_LIST_ITEM_KEY = INV_OPEN_DATA_TAXA.TAXON_LIST_ITEM_KEY
+
+#test <- left_join(invMetrics, invSite, by = "SITE_ID")
 
