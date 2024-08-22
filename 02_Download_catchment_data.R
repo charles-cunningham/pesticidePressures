f859576bc3661e41be09a27a361a4d11f882338c
charles-cunningham <45041242+charles-cunningham@users.noreply.github.com>
2024-08-22 18:01:21 +0100

    Update 02_Download_catchment_data.R

--- 02_Download_catchment_data.R
+++ 02_Download_catchment_data.R
@@ -2,7 +2,7 @@
 #
 # Author: Charles Cunningham
 # 
-# Script Name: Process Biosys data
+# Script Name: Process EA catchment data
 #
 # Script Description:
 
@@ -15,18 +15,16 @@ library(terra)
 ### DATA MANAGEMENT ------------------------------------------------------------
 
 # Set data directory
-# If working on Databricks: "/dbfs/mnt/lab/unrestricted/charles.cunningham@defra.gov.uk/Pesticides/Raw/Species"
-# If working locally: "../Data/Raw/Species"
-dataDir <- "/dbfs/mnt/lab/unrestricted/charles.cunningham@defra.gov.uk/Pesticides/Spatial_data/CatchmentData/"
+# If working on Databricks: "/dbfs/mnt/lab/unrestricted/charles.cunningham@defra.gov.uk/Pesticides/Raw/SpatialData/CatchmentData/"
+# If working locally: "../Data/Raw/Spatial_data/CatchmentData/"
+dataDir <- "/dbfs/mnt/lab/unrestricted/charles.cunningham@defra.gov.uk/Pesticides/Raw/SpatialData/CatchmentData/"
 
 # Create directory if it doesn't exist
 if (!file.exists(dataDir)) {
   dir.create(dataDir, recursive = TRUE)
 }
 
-### LOAD data ------------------------------------------------------------------
-
-### DOWNLOAD BIOSYS DATA -------------------------------------------------------
+### DOWNLOAD CATCHMENT DATA -------------------------------------------------------
 
 # Bulk downloads for entire dataset here:
 # https://environment.data.gov.uk/catchment-planning
