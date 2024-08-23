c45c8589f5266ac36c159e63161e30ab79bb12d9
charles-cunningham <45041242+charles-cunningham@users.noreply.github.com>
2024-08-23 12:49:44 +0100

    Tidy directory structure

--- 02_Download_spatial_data.R
+++ 02_Download_spatial_data.R
@@ -15,15 +15,15 @@ library(terra)
 ### DATA MANAGEMENT ------------------------------------------------------------
 
 # Set data directory
-# If working on Databricks: "/dbfs/mnt/lab/unrestricted/charles.cunningham@defra.gov.uk/Pesticides/Raw/Spatial_data/"
-# If working locally: "../Data/Raw/Spatial_data/"
-dataDir <- "/dbfs/mnt/lab/unrestricted/charles.cunningham@defra.gov.uk/Pesticides/Raw/Spatial_data/"
+# If working on Databricks: "/dbfs/mnt/lab/unrestricted/charles.cunningham@defra.gov.uk/Pesticides/Data/Raw/"
+# If working locally: "../Data/Raw/"
+dataDir <- "/dbfs/mnt/lab/unrestricted/charles.cunningham@defra.gov.uk/Pesticides/Data/Raw/"
 
 # Specify directory for catchment data
-catchmentDir <- paste0(dataDir, "Catchment_data")
+catchmentDir <- paste0(dataDir, "Catchment_data/")
 
 # Specify directory for topgraphic data
-topoDir <- paste0(dataDir, "Topographic_data")
+topoDir <- paste0(dataDir, "Topographic_data/")
 
 # Create directories if they do not exist
 c(catchmentDir, topoDir) %>%
@@ -44,6 +44,25 @@ c(catchmentDir, topoDir) %>%
 # Set download url
 url <- "https://environment.data.gov.uk/catchment-planning/England/shapefile.zip"
 
+# Download
+download.file(url = url,
+              destfile = paste0(catchmentDir, "Catchment_data.zip"),
+              sep = "")
+
+# Unzip
+unzip(paste0(catchmentDir, "Catchment_data.zip"),
+      exdir = catchmentDir)
+
+# Delete .zip files as no longer needed
+unlink(paste0(catchmentDir, "Catchment_data.zip"), recursive = TRUE)
+
+### DOWNLOAD TOPOGRAPHIC DATA --------------------------------------------------
+
+# Bulk downloads for entire dataset here:
+# https://osdatahub.os.uk/downloads/open/Terrain50
+
+url <- "https://api.os.uk/downloads/v1/products/Terrain50/downloads?area=GB&format=ASCII+Grid+and+GML+%28Grid%29&redirect"
+
 # Set file name
 file_name <- "CatchmentData"
 
@@ -56,14 +75,5 @@ unzip(paste0(dataDir, "/", file_name, ".zip"), exdir = dataDir)
 # Delete .zip files as no longer needed
 unlink(paste0(dataDir,
               "CatchmentData.zip"))
-
-### DOWNLOAD TOPOGRAPHIC DATA --------------------------------------------------
-
-
-
-
-
-
-
 
 
