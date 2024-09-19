16a1a3191561d8731b6f6f0267970928f3b2798c
charles-cunningham <45041242+charles-cunningham@users.noreply.github.com>
2024-09-19 17:09:12 +0100

    Update 02_Download_spatial_data.R

--- Scripts/02_Download_spatial_data.R
+++ Scripts/02_Download_spatial_data.R
@@ -19,30 +19,35 @@ library(terra)
 # If working locally: "../Data/Raw/"
 dataDir <- "/dbfs/mnt/lab/unrestricted/charles.cunningham@defra.gov.uk/Pesticides/Data/Raw/"
 
-# Specify directory for surface flow data
-flowDir <- paste0(dataDir, "Flow_data/")
-# Specify directory for catchment data
-catchmentDir <- paste0(dataDir, "Catchment_data/")
-# Specify directory for national boundaries data
-countryDir <- paste0(dataDir, "Country_data/")
-# Specify directory for land cover data
-landDir <-  paste0(dataDir, "Land_cover_data/")
+# Manual dataset download folders
+
 # Specify directory for pesticide data
 pestDir <-  paste0(dataDir, "Pesticide_data/")
 # Specify directory for fertiliser data
 fertDir <-  paste0(dataDir, "Fertiliser_data/")
+# Specify directory for surface flow data
+flowDir <- paste0(dataDir, "Flow_data/")
+# Specify directory for land cover data
+landDir <-  paste0(dataDir, "Land_cover_data/")
+
+# Automated dataset download folders
+
+# Specify directory for catchment data
+catchmentDir <- paste0(dataDir, "Catchment_data/")
+# Specify directory for national boundaries data
+countryDir <- paste0(dataDir, "Country_data/")
 # Specify directory for farming archetype data
 farmDir <-  paste0(dataDir, "Farm_archetype_data/")
 # Specify directory for topographic data
 topoDir <- paste0(dataDir, "Topographic_data/")
 
 # Create directories if they do not exist
-c(flowDir,
+c(pestDir,
+  fertDir,
+  flowDir,
+  landDir,
   catchmentDir,
   countryDir,
-  landDir,
-  pestDir,
-  fertDir,
   farmDir,
   topoDir) %>%
   lapply(., function(x) {
@@ -50,6 +55,20 @@ c(flowDir,
       dir.create(x, recursive = TRUE)
     }
   })
+
+### DOWNLOAD PESTICIDE AND FERTILISER DATA [MANUAL] ----------------------------
+
+# Information on data here:l
+# https://www.ceh.ac.uk/data/ukceh-land-cover-plus-fertilisers-and-pesticides
+
+# This must be ordered through the EIDC data request:
+# https://www.ceh.ac.uk/data-request-form
+
+# Once downloaded:
+# (i) Unzip both pesticide and fertiliser data
+# (ii) Move to 'pestDir' and 'fertDir' respectively
+# (iii) Each directory should be organised in such a way that the respective 
+# data and documentation directories sit directly below 'pestDir' or 'fertDir'
 
 ### DOWNLOAD FLOW DATA [MANUAL] ------------------------------------------------
 
@@ -63,6 +82,20 @@ c(flowDir,
 # (i) .gpkg file is unzipped, then
 # (ii) file is renamed 'Flow_data.gpkg', then 
 # (iii) file is moved to flowDir [>file.copy("../Flow_data.gpkg", flowDir)]
+
+### DOWNLOAD LAND COVER DATA [MANUAL] ------------------------------------------
+
+# Information on data here:
+# https://catalogue.ceh.ac.uk/documents/bb15e200-9349-403c-bda9-b430093807c7
+
+# This must be ordered through the EIDC data request:
+# https://order-eidc.ceh.ac.uk/resources/7RDVVS3B/order
+
+# Once downloaded:
+# (i) File is unzipped
+# (ii) Contents are moved to LandDir
+# (iii) Converted to flat structure (i.e. contents of all directories moved to 
+# 'landDir' and empty directories removed)
 
 ### DOWNLOAD CATCHMENT DATA [AUTOMATED] ----------------------------------------
 
@@ -118,7 +151,7 @@ boundaryEngland <- project(boundaryEngland, "EPSG:27700")
 
 # Save to England folder
 saveRDS(boundaryEngland,
-            file = paste0(countryDir, "England.Rds"))
+        file = paste0(countryDir, "England.Rds"))
 
 # Remove files and directories no longer needed
 unlink(paste0(countryDir, "England.zip"), recursive = TRUE)
@@ -144,7 +177,7 @@ for (i in 1:NROW(boundaries)) {
   
   # Project to British National Grid
   boundary <- project(boundary, "EPSG:27700")
-
+  
   # Save country as vector
   saveRDS(boundary,
           file = paste0(countryDir, country, ".Rds"))
@@ -154,41 +187,24 @@ for (i in 1:NROW(boundaries)) {
          recursive = TRUE)
 }
 
-### DOWNLOAD LAND COVER DATA [MANUAL] ------------------------------------------
-
+### DOWNLOAD FARMING ARCHETYPE DATA [AUTOMATED] --------------------------------
 # Information on data here:
-# https://catalogue.ceh.ac.uk/documents/bb15e200-9349-403c-bda9-b430093807c7
+# https://catalogue.ceh.ac.uk/documents/3b44375a-cbe6-468c-9395-41471054d0f3
 
-# This must be ordered through the EIDC data request:
-# https://order-eidc.ceh.ac.uk/resources/7RDVVS3B/order
+# Download tier 1 archetypes (GB)
+download.file(url = "https://catalogue.ceh.ac.uk/datastore/eidchub/3b44375a-cbe6-468c-9395-41471054d0f3/tier1archetypesgb.tif",
+              destfile = paste0(farmDir, "tier1.tif"),
+              sep = "")
 
-# Once downloaded:
-# (i) .gpkg file is unzipped, then
-# (ii) file is renamed 'Flow_data.gpkg', then 
-# (iii) file is moved to flowDir [>file.copy("../Flow_data.gpkg", flowDir)]
+# Download tier 2 archetypes (GB)
+download.file(url = "https://catalogue.ceh.ac.uk/datastore/eidchub/3b44375a-cbe6-468c-9395-41471054d0f3/tier2archetypesgb.tif",
+              destfile = paste0(farmDir, "tier2.tif"),
+              sep = "")
 
-### DOWNLOAD PESTICIDE AND FERTILISER DATA [MANUAL] ----------------------------
-
-# Information on data here:
-# https://www.ceh.ac.uk/data/ukceh-land-cover-plus-fertilisers-and-pesticides
-
-# This must be ordered through the EIDC data request:
-# https://www.ceh.ac.uk/data-request-form
-
-# Once downloaded:
-# (i) .gpkg file is unzipped, then
-# (ii) file is renamed 'Flow_data.gpkg', then 
-# (iii) file is moved to flowDir [>file.copy("../Flow_data.gpkg", flowDir)]
-
-
-
-
-
-
-### DOWNLOAD FARMING ARCHETYPE DATA [MANUAL] -----------------------------------
-
-
-
+# Download tier 3 archetypes(England)
+download.file(url = "https://catalogue.ceh.ac.uk/datastore/eidchub/3b44375a-cbe6-468c-9395-41471054d0f3/tier3archetypesengland.tif",
+              destfile = paste0(farmDir, "tier3.tif"),
+              sep = "")
 
 ### DOWNLOAD TOPOGRAPHIC DATA [AUTOMATED] --------------------------------------
 
@@ -203,12 +219,12 @@ download.file(url = url,
               destfile = paste0(topoDir, "Topographic_data.zip"),
               sep = "")
 
-# Unzip
+# Unzip data directory and licence.txt to topoDir
 unzip(paste0(topoDir, "Topographic_data.zip"),
       exdir = topoDir,
       overwrite = TRUE)
 
-# Delete .zip files as no longer needed
+# Delete .zip file as no longer needed
 unlink(paste0(topoDir, "Topographic_data.zip"), recursive = TRUE)
 
 ### PROCESS TOPOGRAPHIC DATA ---------------------------------------------------
@@ -240,6 +256,7 @@ writeRaster(topoGB_R,
             filename = paste0(topoDir, "topoGB.asc"),
             overwrite = TRUE)
 
-# Delete dataUnzipped folder, and remove redundant objects
+# Delete data and dataUnzipped folder, and remove redundant objects
+unlink(paste0(topoDir, "data"), recursive = TRUE)
 unlink(paste0(topoDir, "dataUnzipped"), recursive = TRUE)
 rm(topoGB_list, topoGB_R)
