a12d44e28e1dda39e236ed6725a6923296b2617a
charles-cunningham <45041242+charles-cunningham@users.noreply.github.com>
2025-01-28 22:38:29 +0000

    Finish aggregation metrics including diversity metrics

--- Scripts/06_Aggregate_pesticide_data.R
+++ Scripts/06_Aggregate_pesticide_data.R
@@ -4,7 +4,7 @@
 # 
 # Script Name: Aggregate pesticide data
 #
-# Script Description:
+# Script Description: 
 
 ### LOAD LIBRARIES -------------------------------------------------------------
 
@@ -29,17 +29,20 @@ flowChemData <- readRDS(paste0(dataDir,
 pestData <- read.csv(paste0(dataDir,
                             "Raw/Pesticide_data/Pesticide_properties.csv"))
 
-### CREATE TOTAL PESTICIDE LOAD METRIC -----------------------------------------
+### CREATE TOXICITY METRICS -----------------------------------------
 
-# CREATE TOXICITY WEIGHTS
+# SUM
+# Total upstream application of all pesticides in kg
 
-# Create toxicity column to be used for weighted sum  (inverse of NOEC)
-pestData$Toxicity <- 1/
-  pestData$Toxicity_.Temperate_Freshwater_Aquatic_invertebrates_Chronic_21_day_NOEC_.mgl.1..
-
-# Replace NAs with min toxicity value (max NOEC)
-pestData$Toxicity[is.na(pestData$Toxicity)] <- min(pestData$Toxicity,
-                                                   na.rm = TRUE)
+# Create pesticideLoad column as sum of all pesticide application
+flowChemData <- flowChemData %>%
+  mutate(pesticideLoad =
+           # If any pesticide layers are not NA ...
+           # (needed otherwise all NA rows become 0, even with na.rm = TRUE)
+           case_when(if_any(starts_with('pesticide_'),
+                            complete.cases)
+                     # Sum all pesticide values
+                     ~ rowSums(across(starts_with('pesticide_')))))
 
 # CHECK ORDER FOR WEIGHTED SUM
 # (flowData columns have to be in same order as pestData rows to match up)
@@ -58,7 +61,7 @@ pestData$SimpleName <- pestData$Pesticide %>%
   gsub("\\'", "", .) %>%
   gsub(" ", "", .)
 
-# flowData: Create simple name
+# flowData: Create simple pesticide name for matching
 pestData$FlowName <- names(flowChemData) %>%
   .[grepl("pesticide_", .)] %>%
   gsub("pesticide_", "", .) %>%
@@ -66,27 +69,43 @@ pestData$FlowName <- names(flowChemData) %>%
 
 # Check that both match
 if (identical(pestData$SimpleName, pestData$FlowName)) {
-  print("pestData columns match")
+  print("pestData rows and flowData columns match")
 } else {
-  print("not okay")
+  stop("pestData rows and flowData columns do not match")
 }
 
-# Remove cloumns as no longer needed
-pestData$SimpleName <-
-  pestData$FlowName <- NULL
+# Remove columns as no longer needed
+pestData$SimpleName <- pestData$FlowName <- NULL
+
+# CREATE TOXICITY WEIGHTS
+
+# Create toxicity column to be used for weighted sum  (inverse of NOEC)
+toxicity <- 1/
+  pestData$Toxicity_.Temperate_Freshwater_Aquatic_invertebrates_Chronic_21_day_NOEC_.mgl.1..
+
+# Replace NAs with min toxicity value (max NOEC)
+toxicity[is.na(toxicity)] <- min(toxicity,
+                                 na.rm = TRUE)
+
+# Set names of toxicity to flowChem pesticide column names
+# N.B. previously checked order is correct
+names(toxicity) <- grep("pesticide_",
+                         names(flowChemData),
+                         value = TRUE)
 
 # WEIGHTED SUM
 
-# Create pesticideLoad column as sum of all pesticide application
+# Create pesticideToxicLoad column as sum of all pesticide application
 flowChemData <- flowChemData %>%
-  mutate(pesticideLoad =
+  mutate(pesticideToxicLoad =
            # If any pesticide layers are not NA ...
-           # (needed otherwise all NA rows become 0)
+           # (needed otherwise all NA rows become 0, even with na.rm = TRUE)
            case_when(if_any(starts_with('pesticide_'),
                             complete.cases)
                      # Sum all pesticide values
-                     ~ rowSums(across(starts_with('pesticide_')) * 
-                                        pestData$Toxicity,
+                     ~ rowSums(across(starts_with('pesticide_'),
+                                      # Multiply by associated toxicity weights
+                                      ~ .x * toxicity[cur_column()]),
                                na.rm = TRUE)))
 
 ### CREATE PESTICIDE DIVERSITY METRICS ------------------------------------------
@@ -102,7 +121,7 @@ flowChemData <- flowChemData %>%
   # Create pesticideDiv column as Simpson diversity index
   mutate(pesticideSimpson =
            vegan::diversity(across(starts_with('pesticide_')),
-                            index = "Simpson"))
+                            index = "simpson"))
 
 ### SAVE -----------------------------------------------------------------------
 
