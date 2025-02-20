ad908d531f91fd95c4fc9edcb28c0f9b2662fa51
charles-cunningham <45041242+charles-cunningham@users.noreply.github.com>
2025-02-20 12:50:04 +0000

    Update 09_Aggregate_upstream_data.R

--- Scripts/09_Aggregate_upstream_data.R
+++ Scripts/09_Aggregate_upstream_data.R
@@ -2,7 +2,7 @@
 #
 # Author: Charles Cunningham
 # 
-# Script Name: Aggregate pesticide data
+# Script Name: Aggregate upstream data
 #
 # Script Description: 
 
@@ -22,20 +22,20 @@ dataDir <- "/dbfs/mnt/lab/unrestricted/charles.cunningham@defra.gov.uk/Pesticide
 ### LOAD DATA ------------------------------------------------------------------
 
 # Read processed flow data
-flowChemData <- readRDS(paste0(dataDir,
-                               "Processed/Flow/Flow_chem_data.Rds"))
+flowDataAll <- readRDS(paste0(dataDir,
+                               "Processed/Flow/Flow_data_all.Rds"))
 
 # Read pesticide data
 pestData <- read.csv(paste0(dataDir,
                             "Raw/Pesticide_data/Pesticide_properties.csv"))
 
-### CREATE TOXICITY METRICS -----------------------------------------
+### CREATE PESTICIDE METRICS ---------------------------------------------------
 
-# SUM
+### SUM
 # Total upstream application of all pesticides in kg
 
 # Create pesticideLoad column as sum of all pesticide application
-flowChemData <- flowChemData %>%
+flowDataAll <- flowDataAll %>%
   mutate(pesticideLoad =
            # If any pesticide layers are not NA ...
            # (needed otherwise all NA rows become 0, even with na.rm = TRUE)
@@ -43,6 +43,8 @@ flowChemData <- flowChemData %>%
                             complete.cases)
                      # Sum all pesticide values
                      ~ rowSums(across(starts_with('pesticide_')))))
+
+### WEIGHTED SUM
 
 # CHECK ORDER FOR WEIGHTED SUM
 # (flowData columns have to be in same order as pestData rows to match up)
@@ -62,10 +64,20 @@ pestData$SimpleName <- pestData$Pesticide %>%
   gsub(" ", "", .)
 
 # flowData: Create simple pesticide name for matching
-pestData$FlowName <- names(flowChemData) %>%
+pestData$FlowName <- names(flowDataAll) %>%
   .[grepl("pesticide_", .)] %>%
   gsub("pesticide_", "", .) %>%
-  gsub("\\.", "", .)
+  gsub("\\(", "", .) %>%
+  gsub("\\)", "", .) %>%
+  gsub("\\[", "", .) %>%
+  gsub("\\]", "", .) %>%
+  gsub("\\-", "", .) %>%
+  gsub("\\,", "", .) %>%
+  gsub("\\.", "", .) %>%
+  gsub("\\/", "", .) %>%
+  gsub("\\&", "", .) %>%
+  gsub("\\'", "", .) %>%
+  gsub(" ", "", .)
 
 # Check that both match
 if (identical(pestData$SimpleName, pestData$FlowName)) {
@@ -90,13 +102,13 @@ toxicity[is.na(toxicity)] <- min(toxicity,
 # Set names of toxicity to flowChem pesticide column names
 # N.B. previously checked order is correct
 names(toxicity) <- grep("pesticide_",
-                         names(flowChemData),
+                         names(flowDataAll),
                          value = TRUE)
 
-# WEIGHTED SUM
+# CALCULATE WEIGHTED SUM
 
 # Create pesticideToxicLoad column as sum of all pesticide application
-flowChemData <- flowChemData %>%
+flowDataAll <- flowDataAll %>%
   mutate(pesticideToxicLoad =
            # If any pesticide layers are not NA ...
            # (needed otherwise all NA rows become 0, even with na.rm = TRUE)
@@ -108,10 +120,10 @@ flowChemData <- flowChemData %>%
                                       ~ .x * toxicity[cur_column()]),
                                na.rm = TRUE)))
 
-### CREATE PESTICIDE DIVERSITY METRICS ------------------------------------------
+### CREATE PESTICIDE DIVERSITY METRICS -----------------------------------------
 
 # Create diversity indices
-flowChemData <- flowChemData %>%
+flowDataAll <- flowDataAll %>%
   
   # Create pesticideDiv column as Shannon diversity index
   mutate(pesticideShannon =
@@ -123,9 +135,15 @@ flowChemData <- flowChemData %>%
            vegan::diversity(across(starts_with('pesticide_')),
                             index = "simpson"))
 
+# REMOVE REDUNDANT PESTICIDE COLUMNS -------------------------------------------
+
+# Select all invividual pesticide columns and remove as not needed (aggreagted)
+flowDataAll <- flowDataAll %>%
+  select (-c(starts_with('pesticide_')))
+
 ### SAVE -----------------------------------------------------------------------
 
 # Save processed flow data
-saveRDS(flowChemData,
+saveRDS(flowDataAll,
         file = paste0(dataDir,
                       "/Processed/Flow/Flow_aggregated_data.Rds"))
