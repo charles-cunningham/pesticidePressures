753eae8335f84396ff5a03550fa436024240c7de
charles-cunningham <45041242+charles-cunningham@users.noreply.github.com>
2025-10-28 11:35:22 +0000

    Update 11_Aggregate_upstream_data.R

--- Scripts/11_Aggregate_upstream_data.R
+++ Scripts/11_Aggregate_upstream_data.R
@@ -45,9 +45,9 @@ flowDataAll <- flowDataAll %>%
                      # Sum all pesticide values
                      ~ rowSums(across(starts_with('pesticide_')))))
 
-### WEIGHTED SUM
+### WEIGHTED SUMS
 
-# CHECK ORDER FOR WEIGHTED SUM
+# CHECK ORDER FOR WEIGHTS
 # (flowData columns have to be in same order as pestData rows to match up)
 
 # pestData; Create simple pesticide name for matching
@@ -92,7 +92,7 @@ pestData$SimpleName <- pestData$FlowName <- NULL
 
 # CREATE TOXICITY WEIGHTS
 
-# Create toxicity column to be used for weighted sum  (inverse of NOEC)
+# Create toxicity vector to be used for weighted sum  (inverse of NOEC)
 toxicity <- 1/
   pestData$Toxicity_.Temperate_Freshwater_Aquatic_invertebrates_Chronic_21_day_NOEC_.mgl.1..
 
@@ -100,13 +100,31 @@ toxicity <- 1/
 toxicity[is.na(toxicity)] <- min(toxicity,
                                  na.rm = TRUE)
 
+# CREATE LEACHING WEIGHTS
+
+# Create leachability vector
+leachability <- pestData$Leachability..GUS_leaching_potential_index.
+
+# Remove values under 0 (extremely low leaching potential) and NA values
+leachability[leachability < 0 | is.na(leachability)] <- 0
+
+# CREATE -CIDE WEIGHTS
+
+insecticide <- pestData$Insecticide
+herbicide <- pestData$Herbicide
+fungicide <- pestData$Fungicide
+
+# SET NAMES
+
 # Set names of toxicity to flowChem pesticide column names
 # N.B. previously checked order is correct
-names(toxicity) <- grep("pesticide_",
-                         names(flowDataAll),
-                         value = TRUE)
+names(toxicity) <- names(leachability) <-
+  names(insecticide) <- names(herbicide) <- names(fungicide) <-
+  grep("pesticide_",
+       names(flowDataAll),
+       value = TRUE)
 
-# CALCULATE WEIGHTED SUM
+# CALCULATE WEIGHTED SUMS
 
 # Create pesticideToxicLoad column as sum of all pesticide application
 flowDataAll <- flowDataAll %>%
@@ -118,7 +136,57 @@ flowDataAll <- flowDataAll %>%
                      # Sum all pesticide values
                      ~ rowSums(across(starts_with('pesticide_'),
                                       # Multiply by associated toxicity weights
-                                      ~ .x * toxicity[cur_column()]),
+                                      ~ .x * 
+                                        toxicity[cur_column()] *
+                                        leachability[cur_column()] ),
+                               na.rm = TRUE)))
+
+# Create insecticideToxicLoad column as sum of insecticide application
+flowDataAll <- flowDataAll %>%
+  mutate(insecticideToxicLoad =
+           # If any pesticide layers are not NA ...
+           # (needed otherwise all NA rows become 0, even with na.rm = TRUE)
+           case_when(if_any(starts_with('pesticide_'),
+                            complete.cases)
+                     # Sum all pesticide values
+                     ~ rowSums(across(starts_with('pesticide_'),
+                                      # Multiply by associated toxicity weights
+                                      ~ .x * 
+                                        toxicity[cur_column()] *
+                                        leachability[cur_column()] *
+                                        insecticide[cur_column()] ),
+                               na.rm = TRUE)))
+
+# Create insecticideToxicLoad column as sum of herbicide application
+flowDataAll <- flowDataAll %>%
+  mutate(herbicideToxicLoad =
+           # If any pesticide layers are not NA ...
+           # (needed otherwise all NA rows become 0, even with na.rm = TRUE)
+           case_when(if_any(starts_with('pesticide_'),
+                            complete.cases)
+                     # Sum all pesticide values
+                     ~ rowSums(across(starts_with('pesticide_'),
+                                      # Multiply by associated toxicity weights
+                                      ~ .x * 
+                                        toxicity[cur_column()] *
+                                        leachability[cur_column()] *
+                                        herbicide[cur_column()] ),
+                               na.rm = TRUE)))
+
+# Create insecticideToxicLoad column as sum of all fungicide application
+flowDataAll <- flowDataAll %>%
+  mutate(fungicideToxicLoad =
+           # If any pesticide layers are not NA ...
+           # (needed otherwise all NA rows become 0, even with na.rm = TRUE)
+           case_when(if_any(starts_with('pesticide_'),
+                            complete.cases)
+                     # Sum all pesticide values
+                     ~ rowSums(across(starts_with('pesticide_'),
+                                      # Multiply by associated toxicity weights
+                                      ~ .x * 
+                                        toxicity[cur_column()] *
+                                        leachability[cur_column()] *
+                                        fungicide[cur_column()] ),
                                na.rm = TRUE)))
 
 ### CREATE PESTICIDE DIVERSITY METRICS -----------------------------------------
