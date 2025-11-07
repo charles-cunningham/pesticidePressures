cbdee937eb55d381e1d0cef692b49525211c1cc7
charles-cunningham <45041242+charles-cunningham@users.noreply.github.com>
2025-11-07 09:48:34 +0000

    Update 13_Process_Biosys_for_modelling.R

--- Scripts/13_Process_Biosys_for_modelling.R
+++ Scripts/13_Process_Biosys_for_modelling.R
@@ -138,17 +138,17 @@ modelVariables <- c(
   "EDF_MEAN",
   "HS_HMS_RSB_SubScore",
   "HS_HQA",
-  #"ALTITUDE",
-  #"SLOPE",
-  #"DIST_FROM_SOURCE",
-  #"DISCHARGE",
-  #"WIDTH",
-  #"DEPTH",
+  "ALTITUDE",
+  "SLOPE",
+  "DIST_FROM_SOURCE",
+  "DISCHARGE",
+  "WIDTH",
+  "DEPTH",
   "BOULDERS_COBBLES",
   "PEBBLES_GRAVEL",
   "SAND",
-  "SILT_CLAY" #,
- # "ALKALINITY"
+  "SILT_CLAY",
+  "ALKALINITY"
  )
 
 # Create additional scaled column for each modelVariables
@@ -180,35 +180,35 @@ invData$WATER_BODY_F <-paste( invData$REPORTING_AREA,
 
 ### CONVERT RW EFFECTS TO DISCRETE DATA
 
-# Loop through site variables used for random effects
-for (i in c("ALTITUDE",
-            "SLOPE",
-            "DIST_FROM_SOURCE",
-            "DISCHARGE",
-            "ALKALINITY")) {
-# Group to 10 evenly sized bins and assign to new column
-  invData[, paste0(i, "_GRP")] <- INLA::inla.group(invData[[i]],
-                                                  n = 10, 
-                                                  method = "cut")
-  
-}
+# # Loop through site variables used for random effects
+# for (i in c("ALTITUDE",
+#             "SLOPE",
+#             "DIST_FROM_SOURCE",
+#             "DISCHARGE",
+#             "ALKALINITY")) {
+# # Group to 10 evenly sized bins and assign to new column
+#   invData[, paste0(i, "_GRP")] <- INLA::inla.group(invData[[i]],
+#                                                   n = 10, 
+#                                                   method = "cut")
+#   
+# }
 
 ### CONVERT SAMPLING SITE ABIOTIC VARIABLES TO PCA ----------------------------------------------
 
 # Carry out PCA
 sitePCA <- invData %>%
   as_tibble(.) %>%
-  select(#ALTITUDE_scaled,
-         #SLOPE_scaled,
-         #DIST_FROM_SOURCE_scaled,
-         #DISCHARGE_scaled,
-         #WIDTH_scaled,
-         #DEPTH_scaled,
+  select(ALTITUDE_scaled,
+         SLOPE_scaled,
+         DIST_FROM_SOURCE_scaled,
+         DISCHARGE_scaled,
+         WIDTH_scaled,
+         DEPTH_scaled,
          BOULDERS_COBBLES_scaled,
          PEBBLES_GRAVEL_scaled,
          SAND_scaled,
          SILT_CLAY_scaled,
-         #ALKALINITY_scaled
+         ALKALINITY_scaled
          ) %>%
   prcomp(.)
 
@@ -217,6 +217,30 @@ sitePCA; summary(sitePCA)
 
 # Join to invData
 invData <- cbind(invData, sitePCA$x)
+
+###  PCA evaluation
+# Code from https://en.proft.me/2016/11/15/principal-component-analysis-pca-r/)
+
+evplot = function(ev) {
+  # Broken stick model (MacArthur 1957)
+  n = length(ev)
+  bsm = data.frame(j=seq(1:n), p=0)
+  bsm$p[1] = 1/n
+  for (i in 2:n) bsm$p[i] = bsm$p[i-1] + (1/(n + 1 - i))
+  bsm$p = 100*bsm$p/n
+  # Plot eigenvalues and % of variation for each axis
+  op = par(mfrow=c(2,1),omi=c(0.1,0.3,0.1,0.1), mar=c(1, 1, 1, 1))
+  barplot(ev, main="Eigenvalues", col="bisque", las=2)
+  abline(h=mean(ev), col="red")
+  legend("topright", "Average eigenvalue", lwd=1, col=2, bty="n")
+  barplot(t(cbind(100*ev/sum(ev), bsm$p[n:1])), beside=TRUE, 
+          main="% variation", col=c("bisque",2), las=2)
+  legend("topright", c("% eigenvalue", "Broken stick model"), 
+         pch=15, col=c("bisque",2), bty="n")
+  par(op)
+}
+ev = sitePCA$sdev^2
+evplot(ev)
 
 ### CORRELATION PLOTS WITH ALL VARIABLES ----------------------------------------------------------
 
@@ -245,11 +269,8 @@ corr_df <- invData %>%
          HS_HQA_scaled,
          PC1,
          PC2,
-         # PC3,
-         # PC4,
-         # PC5,
-         # PC6,
-         # PC8,
+         PC3,
+         PC4,
          YEAR,
          MONTH_NUM)
 
@@ -332,12 +353,8 @@ corr_df <- invData %>%
          totalArea_scaled,
          PC1,
          PC2,
-        #  PC3,
-         # PC4,
-         # PC5,
-         # PC6,
-         # PC7,
-         # PC8,
+         PC3,
+         PC4,
          YEAR,
          MONTH_NUM)
 
